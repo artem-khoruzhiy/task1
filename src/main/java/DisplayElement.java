88c522f499d39eb16ca1934dbbcabe5c71031a1b
Arte Khoruzhiy <artem_juventini@mail.ru>
2017-03-17 10:35:34 +0300

    Initial commit

---
+++ src/main/java/DisplayElement.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Артем on 06.03.2017.
+ */
+public interface DisplayElement {
+    public void display();
+}
---
+++ src/main/java/Observer.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Артем on 06.03.2017.
+ */
+public interface Observer {
+    public void update(float temp, float humidity, float pressure);
+}
